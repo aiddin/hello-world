0458c47af936fa60eaf56d1d57d8c8037b3d56c7
aiddin <arcticdessert@gmail.com>
2022-09-01 17:49:46 +0800

    first commit

---
+++ src/components/alienClock.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="test">
+    <h1>{{ msg }}</h1>
+    <h2>{{time}}</h2>
+    <h3>{{alienTime}}</h3>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'alienClock',
+  props: {
+    msg: String,
+    time: Number,
+    alienTime: Number
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
